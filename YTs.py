8400d13c6211414efc03af6433bd5ad0caa2f55a
DBFTS042 <153144972+DBFTS042@users.noreply.github.com>
2024-10-21 03:42:59 -0400

    Test_YTs.py

--- YTs.py
+++ YTs.py
@@ -1,4 +1,4 @@
 import subprocess
 
-playlist_url = "YOUR_YOUTUBE_PLAYLIST_URL"
+playlist_url = "https://youtube.com/playlist?list=PLQYJ8WuT00qw4g81AfDUHOvy8ZkJvhU07&si=NrnRa0IH12lK_NbQ"
 subprocess.call(['youtube-dl', '-f', 'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--output', '%(title)s.%(ext)s', playlist_url])
