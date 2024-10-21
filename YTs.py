9ee073db9f8023befda0639bbbdd202ca5ddbbca
DBFTS042 <153144972+DBFTS042@users.noreply.github.com>
2024-10-21 03:40:47 -0400

    YTs.py

--- YTs.py
+++ YTs.py
@@ -1,30 +1,4 @@
-import youtube_dl
+import subprocess
 
-# Ask for the YouTube URL from the user
-url = input("Enter the YouTube URL link: ")
-
-# Ask the user to choose a download format
-print("Choose a download format:")
-print("1 = .mp3")
-print("2 = .mp4")
-print("3 = .avi")
-print("4 = .gif")
-format_choice = input("Enter the format number: ")
-
-# Define the download format based on user choice
-if format_choice == "1":
-    download_format = "bestaudio/best"
-elif format_choice == "2":
-    download_format = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
-elif format_choice == "3":
-    download_format = "best[ext=avi]"
-elif format_choice == "4":
-    download_format = "best[ext=gif]"
-
-# Download the video based on user input
-ydl_opts = {
-    'format': download_format,
-}
-
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+playlist_url = "YOUR_YOUTUBE_PLAYLIST_URL"
+subprocess.call(['youtube-dl', '-f', 'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--output', '%(title)s.%(ext)s', playlist_url])
